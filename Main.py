a189ebc04bf9cade9819a0cb175e9f4c2ccfa6f2
saidiya1021 <109952168+saidiya1021@users.noreply.github.com>
2022-10-10 00:58:16 +0530

    commit1

--- Main.py
+++ Main.py
@@ -1,7 +1,22 @@
 from typing import List
 
 def quick_sort(data, low, high) -> List[int]:
-    # Write code here
+     i = ( low-1 )
+   piv = data[high]               
+   for j in range(low , high):
+      if data[j] <= piv:
+         i = i+1
+         data[i],data[j] = data[j],data[i]
+   data[i+1],data[high] = data[high],data[i+1]
+   return ( i+1 )
+
+def quick_sort(data,low,high):
+   if low < high:
+      
+      piv = partition(data,low,high)
+      quick_sort(data, low, pi-1)
+      quick_sort(data, pi+1, high)
+   return data
 
 
 input_data = input()
